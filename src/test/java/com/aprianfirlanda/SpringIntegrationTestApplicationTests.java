0b276544737d5450537b154110bc851378195d06
aprianfirlanda <aprianfirlanda@gmail.com>
2023-04-13 21:23:10 +0700

    init commit from start.spring.io

---
+++ src/test/java/com/aprianfirlanda/SpringIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprianfirlanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
